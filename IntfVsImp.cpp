3927a8048ea896ad93340232bdd4eee4a2166d3c
Mackin03 <lasalagemmarc03@gmail.com>
2024-09-11 17:52:30 +0800

    add library making exercise

---
+++ IntfVsImp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "mikemath.hpp" //always include 'header files' never .cpp files
+
+// mikemath.cpp
+//mikemath.hpp
+
+int main(){
+
+    std::cout << add(7,2) << std::endl;
+    return 0;
+}
