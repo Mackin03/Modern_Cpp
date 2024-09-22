155f144e38362c7f4c5729f3706ba862ed2c06a9
Mc_belly <lasalagemmarc03@gmail.com>
2024-09-22 15:57:13 +0800

    add classes3

--- classes3.cpp
+++ classes3.cpp
@@ -1,18 +1,69 @@
 #include <iostream>
 #include <string>
 
-class Vector3{
+class Array{
     public:
-        float x,y,z;
+        Array(){
+            std::cout << "Constructor" << std::endl;
+            data = new int[10];
+            for (int i=0 ; i<10 ; i++){
+                data[i] = i*i;
+            }
+        }
+        ~Array(){
+            delete[] data;
+        }
+        //Copy Constructor
+        Array(const Array& rhs){
+            std::cout << "Copy Constructor" << std::endl;
+            data = new int[10];
+            for(int i=0 ; i<10 ;i++){
+                data[i] = rhs.data[i];
+            }
+        }
+        // Copy assignment operator
+        // Object is already constructed, we are just making a copy later (e.g. myArray2 = myArray)
+
+        Array& operator = (const Array& rhs){
+            std::cout << "Copy Assignment operator" << std::endl;
+
+            if (&rhs == this){
+                return *this;
+            }
+            delete[] data;
+            data = new int[10];
+            for (int i=0 ; i<10 ; i++){
+                data[i] = rhs.data[i];
+            }
+            return *this; // refers actually to the object itself
+        }
+
+        void PrintData(){
+            for (int i=0 ; i<10 ; i++){
+                std::cout << data[i] << std::endl;
+            }
+        }
+
+        void SetData(int index, int value){
+            data[index] = value;
+        }
+    private:
+        int* data;
+
 };
 
 int main(){
-    Vector3 myVector;
-    myVector.x = 1.0f;
-    myVector.y = 2.0f;
-    myVector.z = 3.0f;
-    Vector3 myVector2 = myVector;
 
-    std::cout << myVector2.x << "," << myVector2.y << "," << myVector2.z << std::endl;
+    // Initialize all of our data up front
+    Array myArray;
+
+    myArray.SetData(0,100000);
+    myArray.SetData(1,77);
+    myArray.SetData(2,999);
+
+    Array myArray2;
+    myArray2 = myArray;
+    myArray.PrintData();
+    myArray2.PrintData();
     return 0;
 }
